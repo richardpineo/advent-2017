8062a07930bcb302f9253ae487f01e2731fb3417
Richard Pineo <Richard.E.Pineo@ge.com>
2017-12-09 10:20:40 -0800

    Init repo

---
+++ 3a.js
@@ -0,0 +1,13 @@
+/*jshint esversion: 6 */
+
+const val = 347991;
+
+for(let v=1;; v++) {
+    if( )
+}
+
+
+console.log(`${val} in ring ${ring}`);
+console.log(`${ring}^2 = ${ring*ring}`);
+
+
---
+++ readme.md
@@ -0,0 +1,2 @@
+http://adventofcode.com/
+
