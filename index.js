3b7fb61482d8c17a849082495eb8e20b17370d80
Richard Pineo <Richard.E.Pineo@ge.com>
2017-12-09 14:04:23 -0800

    add lockfile?

--- index.js
+++ index.js
@@ -2,38 +2,41 @@ var colors = require('colors');
 
 console.log("starting up".green)
 
-console.log("\n1a".red);
-require("./1a");
-
-console.log("\n1b".red);
-require("./1b");
-
-console.log("\n2a".red);
-require("./2a");
-
-console.log("\n2b".red);
-require("./2b");
-
-console.log("\n3a".red);
-require("./3a");
-
-console.log("\n4a".red);
-require("./4a");
-
-console.log("\n4b".red);
-require("./4b");
-
-console.log("\n5a".red);
-require("./5a");
-
-console.log("\n5b".red);
-require("./5b");
-
-console.log("\n6a".red);
-require("./6a");
-
-console.log("\n6b".red);
-require("./6b");
+let all = false;
+if( all ) {
+    console.log("\n1a".red);
+    require("./1a");
+    
+    console.log("\n1b".red);
+    require("./1b");
+    
+    console.log("\n2a".red);
+    require("./2a");
+    
+    console.log("\n2b".red);
+    require("./2b");
+    
+    console.log("\n3a".red);
+    require("./3a");
+    
+    console.log("\n4a".red);
+    require("./4a");
+    
+    console.log("\n4b".red);
+    require("./4b");
+    
+    console.log("\n5a".red);
+    require("./5a");
+    
+    console.log("\n5b".red);
+    require("./5b");
+    
+    console.log("\n6a".red);
+    require("./6a");
+    
+    console.log("\n6b".red);
+    require("./6b");
+}
 
 
 console.log("\nshutting down".green)
