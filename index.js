475c87a07d4afb88cd9a432e82bfd9e882ae3f7a
Richard Pineo <Richard.E.Pineo@ge.com>
2017-12-09 10:41:35 -0800

    Adding stuff

---
+++ index.js
@@ -0,0 +1,23 @@
+var colors = require('colors');
+
+console.log("starting up".green)
+
+console.log("\n1a".red);
+require("./1a");
+
+console.log("\n1b".red);
+require("./1b");
+
+console.log("\n2a".red);
+require("./2a");
+
+console.log("\n2b".red);
+require("./2b");
+
+console.log("\n3a".red);
+require("./3a");
+
+
+console.log("\nshutting down".green)
+
+process.exit();
