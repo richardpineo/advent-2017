028873037e4684839368162ab8f22af78b2f4993
Richard Pineo <Richard.E.Pineo@ge.com>
2017-12-12 23:26:43 -0800

    13b

--- 13b.js
+++ 13b.js
@@ -3,39 +3,28 @@
 var fs = require('fs');
 var steps = fs.readFileSync('./data/13-input.txt').toString().split("\n");
 console.log(`${steps.length} steps read`);
+let _ = require("lodash");
 
 const regex = /^(\d+): (\d+)$/;
 
 const MaxSlot = 84;
 
-let scanners = steps.map((s) => {
-    let result = regex.exec(s);
-    if(result.length !== 3) {
-        throw("nope");
-    }
-
-    return {
-        slot: parseInt(result[1]),
-        range: parseInt(result[2])
-    };
-});
-
-let moveScanners = function() {
-    scanners.forEach((s) => {
-        if( s.increasing ) {
-            if( s.position === s.range - 1) {
+let moveScanners = function (scans) {
+    scans.forEach((s) => {
+        if (s.increasing) {
+            if (s.position === s.range - 1) {
                 s.position--;
                 s.increasing = false;
-            } 
+            }
             else {
                 s.position++;
             }
         }
         else {
-            if( s.position === 0 ) {
+            if (s.position === 0) {
                 s.position++;
                 s.increasing = true;
-            } 
+            }
             else {
                 s.position--;
             }
@@ -43,37 +32,56 @@ let moveScanners = function() {
     });
 };
 
-let wasDetected = function(slot) {
-    let scanner = scanners.find(s => s.slot == slot);
+let wasDetected = function (scans, slot) {
+    let scanner = scans.find(s => s.slot == slot);
     return scanner && !scanner.position;
 };
 
-let wasCaught = function(delay) {
-    scanners.forEach(s=> {
-        s.position = 0;
-        s.increasing = true;
-    });
+let delay = 0;
 
-    for(let i=0; i<delay; i++) {
-        moveScanners();        
+let scanners = steps.map((s) => {
+    let result = regex.exec(s);
+    if (result.length !== 3) {
+        throw ("nope");
     }
 
-    let severity = 0;
-    for(let slot = 0; slot<=MaxSlot; slot++ ) {
-        if(wasDetected(slot)) {
-            return true;
+    return {
+        slot: parseInt(result[1]),
+        range: parseInt(result[2]),
+        position: 0,
+        increasing: true
+    };
+});
+
+let wasCaught = function (scans) {
+    for (let slot = 0; slot <= MaxSlot; slot++) {
+        if (wasDetected(scans, slot)) {
+            return slot;
         }
-        moveScanners();
+        moveScanners(scans);
     }
-    return false;
+    return -1;
 };
 
-for(let delay=0; delay<10000; delay++) {
-    if(!wasCaught(delay)) {
-        console.log(`Found the path! Delay ${delay} for a good time.`);
+// Yep, this is inefficient as poo.
+let bestSlot = 0;
+console.log('Searching for a safe path. This will take a while...');
+for (; delay < 9999999; delay++) {
+    let caughtAt = wasCaught(_.cloneDeep(scanners));
+    if (caughtAt === -1) {
+        console.log(`\nFound the path! Delay ${delay} for a good time.`);
         return;
     }
+    if (caughtAt > bestSlot) {
+        bestSlot = caughtAt;
+        console.log(`\nBest so far! Detected after ${delay} delay at slot ${caughtAt}`);
+    }
+    moveScanners(scanners);
+    if ((delay % 10000) === 0) {
+        process.stdout.write(".");
+    }
 }
+console.log("");
 console.log(`D'oh! Busted.`);
 
-// 2264
+// 3875838
