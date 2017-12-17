1747781e4b8dd7bdd5e019375dd14d236a87d1a0
Richard Pineo <Richard.E.Pineo@ge.com>
2017-12-17 11:16:32 -0800

    17b

--- 17b-stolen.js
+++ 17b-stolen.js
@@ -1,7 +1,12 @@
 /*jshint esversion: 6 */
 
 // I gave up and stole this one from reddit. oh, the shame
+// https://www.reddit.com/r/adventofcode/comments/7kc0xw/2017_day_17_solutions/
 
+// The eureka moment of this is that becuase we are pulling 
+// from the value after 0, we only need to track what is 
+// inserted at position 1. I didn't figure this out and tried
+// to brute force the whole thing like an idgit.
 
 let steps = 304;
 let count = 5000000;
